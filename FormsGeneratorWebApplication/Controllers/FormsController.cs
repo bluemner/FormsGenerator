1f5d2bfe4fc2db05b65fd0440a00ac9d1187afee
BrandonBluemner <Brandonbluemner@gmail.com>
2015-03-09 18:41:58 -0500

    Program is now set up

---
+++ FormsGeneratorWebApplication/Controllers/FormsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormsGeneratorWebApplication.Controllers
+{
+    public class FormsController : Controller
+    {
+        //
+        // GET: /Forms/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Forms() {
+            return View();
+        }
+	}
+}
