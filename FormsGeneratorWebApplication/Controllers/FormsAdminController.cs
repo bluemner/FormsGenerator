0a0bbb768ac686a74d0416427da782430410738a
BrandonBluemner <Brandonbluemner@gmail.com>
2015-03-16 20:37:35 -0500

    Forms Admin

---
+++ FormsGeneratorWebApplication/Controllers/FormsAdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormsGeneratorWebApplication.Controllers
+{
+    public class FormsAdminController : Controller
+    {
+        //
+        // GET: /FormsAdmin/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
