5c3a60dac96e84610c0bbdb73eae61eebfc495ba
BrandonBluemner <Brandonbluemner@gmail.com>
2015-04-07 18:45:29 -0500

    Demo for template

--- FormsGeneratorWebApplication/Controllers/FormsAdminController.cs
+++ FormsGeneratorWebApplication/Controllers/FormsAdminController.cs
@@ -18,6 +18,8 @@ namespace FormsGeneratorWebApplication.Controllers
         public static int TYPE_TEXT_RADIO = 2;
         public static int TYPE_TEXT_CHECKBOX = 3;
 
+        public static int TEMPLATE_FORM_TRUE_OR_FALSE = 0;
+
 
         private FormsDbContext db = new FormsDbContext();
 
@@ -36,65 +38,125 @@ namespace FormsGeneratorWebApplication.Controllers
         }
 
         [HttpGet]
-        public ActionResult MakeForm() 
+        public ActionResult MakeForm()
         {
 
             return View(new FormsModel());
-        
+
         }
         [HttpPost]
-        public ActionResult MakeForm(FormsModel model){
+        public ActionResult MakeForm(FormsModel model)
+        {
 
 
             var x = 3;
             // This logic works, but when the view passes the model to 
             // this function, the model's FormItemList is null
-           db.FormModels.Add(model);
-           db.SaveChanges();
-           Console.WriteLine("make forms ran");
-               return View("Index");
+            db.FormModels.Add(model);
+            db.SaveChanges();
+            Console.WriteLine("make forms ran");
+            return View("Index");
         }
-        
+
         [HttpGet]
-        public ActionResult AddTextBox(int count) {
+        public ActionResult AddTextBox(int count)
+        {
             ViewBag.TextBoxCount = count.ToString();
             return PartialView("_EditTextBoxPartial", new TextBoxModel());
         }
 
-       [HttpGet]
-        public ActionResult AddTextArea(int count) {
+        [HttpGet]
+        public ActionResult AddTextArea(int count)
+        {
             ViewBag.TextBoxCount = count;
             return PartialView("_TextAreaPartial", new TextAreaModel() { type = TYPE_TEXT_AREA });
         }
 
-       [HttpGet]
-       public ActionResult AddCheckBoxes(int count, int numberOfSubElements)
-       {
-           var CheckboxModel = new CheckBoxesModel();
-           CheckboxModel.checkboxes = new List<checkbox>();
+        [HttpGet]
+        public ActionResult AddCheckBoxes(int count, int numberOfSubElements)
+        {
+            var CheckboxModel = new CheckBoxesModel();
+            CheckboxModel.checkboxes = new List<checkbox>();
 
-           for(int i=0; i< numberOfSubElements; ++i){
-              CheckboxModel.checkboxes.Add(new checkbox());
-           }
-           
-           ViewBag.TextBoxCount = count;
-           return PartialView("_CheckBoxesPartial",CheckboxModel);
-       }
+            for (int i = 0; i < numberOfSubElements; ++i)
+            {
+                CheckboxModel.checkboxes.Add(new checkbox());
+            }
 
-       [HttpGet]
-       public ActionResult AddRadioButton(int count, int numberOfSubElements)
-       {
-           var RadioMod = new RadioButtonModel();
-           RadioMod.options = new List<string>();
+            ViewBag.TextBoxCount = count;
+            return PartialView("_CheckBoxesPartial", CheckboxModel);
+        }
 
-           for (int i = 0; i < numberOfSubElements; ++i)
-           {
-               RadioMod.options.Add("option"+i);
-           }
+        [HttpGet]
+        public ActionResult AddRadioButton(int count, int numberOfSubElements)
+        {
+            var RadioMod = new RadioButtonModel();
+            RadioMod.options = new List<string>();
 
-           ViewBag.TextBoxCount = count;
-           return PartialView("_RadioButtonPartial", RadioMod);
-       }
+            for (int i = 0; i < numberOfSubElements; ++i)
+            {
+                RadioMod.options.Add("option" + i);
+            }
+            RadioMod.question = "Can pigs fly?";
 
-	}
+            ViewBag.TextBoxCount = count;
+            return PartialView("_RadioButtonPartial", RadioMod);
+        }
+
+        [HttpGet]
+        public ActionResult Template(int type, int numberOfSubElements)
+        {
+            ViewBag.TextBoxCount = numberOfSubElements;
+            // Create instance of a form
+            var fromModel = new FormsModel();
+            switch (type)
+            {
+                case 0:
+                    // populate form model with true or false
+                    for (int i = 0; i < numberOfSubElements; ++i)
+                    {
+                        var RadioMod = new RadioButtonModel();
+                        RadioMod.options = new List<string>();
+
+                        RadioMod.options.Add("True");
+                        RadioMod.options.Add("False");
+
+                        RadioMod.question = "Can pigs fly?";
+                        fromModel.FormItemIList.Add(RadioMod);
+                    }
+                    return View(fromModel);
+            
+
+            }
+            return PartialView("Error");
+        }
+
+        [HttpGet]
+        public ActionResult CopyTemplate(int type, int numberOfSubElements)
+        {
+            ViewBag.TextBoxCount = numberOfSubElements;
+            // Create instance of a form
+            var fromModel = new FormsModel();
+            switch (type)
+            {
+                case 0:
+
+                    // populate form model with true or false
+                    for (int i = 0; i < numberOfSubElements; ++i)
+                    {
+                        var RadioMod = new RadioButtonModel();
+                        RadioMod.options = new List<string>();
+
+                        RadioMod.options.Add("True");
+                        RadioMod.options.Add("False");
+
+                        RadioMod.question = "Can pigs fly?";
+                        fromModel.FormItemIList.Add(RadioMod);
+                    }
+                    return View(fromModel);
+            }
+            return PartialView("Error");
+        }
+
+    }
 }
