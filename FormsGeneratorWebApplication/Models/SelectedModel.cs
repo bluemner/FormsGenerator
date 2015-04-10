a34e3c3fb3b43126e6a37107d2238bcb6ec03b90
Jakusz <Jakusz.nate@gmail.com>
2015-04-10 12:21:47 -0500

    added selected model, updated dbcontext

---
+++ FormsGeneratorWebApplication/Models/SelectedModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace FormsGeneratorWebApplication.Models
+{
+    public class SelectedModel
+    {
+        [Key]
+        public int key { get; set; }
+
+        //this is the string for the selected option that this model represents
+        public virtual string selected { get; set; }
+
+        //this is the question that this selected option belongs to
+        public virtual FormItemModel question { get; set; }
+    }
+}
