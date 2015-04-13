7f9b59e10047a9c11fc355e7b181927e0bbcb8a1
Dipen Patel <dapatel@uwm.edu>
2015-04-12 21:24:01 -0500

    a

---
+++ FormsGeneratorWebApplication/Utilities/DownloadFileActionResult.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Web.Mvc;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace FormsGeneratorWebApplication.Utilities
+{
+    public class DownloadFileActionResult : ActionResult
+    {
+        public GridView gridView = new GridView();
+        public string fileName;
+
+        public DownloadFileActionResult(DataTable dt, string fileName)
+        {
+            gridView.DataSource = dt;
+            gridView.DataBind();
+            this.fileName = fileName;
+        }
+
+        public override void PerformResult(ControllerContext context)
+        {
+            HttpContext currentContext = HttpContext.Current;
+            currentContext.Response.Clear();
+            currentContext.Response.AddHeader("content-disposition", "attachment;filename=" + this.fileName);
+            currentContext.Response.Charset = "";
+            currentContext.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            currentContext.Response.ContentType = "application/vnd.ms-excel";
+
+            StringWriter writer = new StringWriter();
+            HtmlTextWriter htmlWriter = new HtmlTextWriter(writer);
+            gridView.RenderControl(htmlWriter);
+
+            currentContext.Response.Output.Write(writer.ToString());
+            currentContext.Response.Flush();
+            currentContext.Response.End();
+        }
+    }
+}
