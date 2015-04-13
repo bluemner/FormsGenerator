c6a47fd114da9c67af9dc03c1b7cdbfa0b9a2f2f
uniquesingh <sukh286@gmail.com>
2015-04-13 00:52:44 -0500

    Email JS and Remove

--- FormsGeneratorWebApplication/Scripts/views/forms-admin-email.js
+++ FormsGeneratorWebApplication/Scripts/views/forms-admin-email.js
@@ -23,6 +23,13 @@
             $email.on('click', '#email-submit-btn', function () {
                 form.sub();
             });
+
+
+            $email.on('click', '#email-remove-btn', function () {
+                form.removeEmailItem();
+            });
+
+
             
         },
         sub: function(){
@@ -60,6 +67,24 @@
 
          
         },
+
+        removeEmailItem: function () {
+            var emailToRemove = $('#email-remove-text').val();
+            var index = form.Email.indexOf(emailToRemove);
+            if (index > -1) {
+                form.Email.splice(index, 1);
+             
+                var txt = "";
+                for (var i = 0; i < form.Email.length; ++i) {
+                    if (i == 0) {
+                        txt += form.Email[i];
+                    } else {
+                        txt += ",\n" + form.Email[i];
+                    }
+                }
+                $('.email-text-area').val(txt);
+            }
+        }
 
     });//extend
 })(window.jQuery, window.form || (window.form = {}));
