7fb115a3ba2e9426e95451179ac0326ecb9e05f1
Jakusz <Jakusz.nate@gmail.com>
2015-04-22 09:36:20 -0500

    [authorize] and CreateDataBaseIfNotExists

--- FormsGeneratorWebApplication/Controllers/FormsAdminController.cs
+++ FormsGeneratorWebApplication/Controllers/FormsAdminController.cs
@@ -17,6 +17,7 @@ using System.Threading.Tasks;
 namespace FormsGeneratorWebApplication.Controllers
 
 {
+    [Authorize]
     public class FormsAdminController : Controller
     {
         public static int TYPE_TEXT_BOX = 0;
